8e955dc1cf145f73086eb39e425ecd5de4c41726
YH_hao_are_you <mlapenang1998@gmail.com>
2024-07-19 22:43:45 +0900

    feat: Initialize conditions from variables

--- Assets/_ExpWorld_/Scripts/StateManagement/ConditionManager.js
+++ Assets/_ExpWorld_/Scripts/StateManagement/ConditionManager.js
@@ -1,6 +1,6 @@
-const variables = [
-    { name: "color", values: ["R", "G", "B"], isRandomized: false },
-    { name: "size", values: [5, 10], isRandomized: true }
+const variables_dummy = [
+    { name: "color", values: ["R", "G", "B"], isRandom: false },
+    { name: "size", values: [5, 10, 15], isRandom: true },
 ];
 
 const trialsCountForEachCondition = 1;
@@ -33,14 +33,108 @@ $.onReceive((messageType, arg, sender) => {
     }
 });
 
-// Initialize conditions from variables
 function InitConditions () {
     $.state.isLast = false;
 
-    // TODO: replace the below line with the following: decide trial count & conditions in each trial (random or in order) by within-subject independent variables
-    $.state.conditions = [ { color: "R", size: 5 }, { color: "R", size: 10 }, { color: "G", size: 5 }, { color: "G", size: 10 }, { color: "B", size: 10 }, { color: "B", size: 5 } ];
+    $.state.conditions = getConditions(variables_dummy);
     
     $.state.conditionDependentObjects.forEach(obj => {
-        obj.send("exp_updateConditions", $.state.conditions );
+        obj.send("exp_updateConditions", $.state.conditions);
     });
+}
+
+// --------- Initialize conditions from variables ----------
+
+function getConditions (variables) {
+    const repeatPerCombination = 2;
+    const isRepetitionRandom = true;
+    
+    const allCombinations = generateCombinations(variables);
+    
+    // Group by non-random variables
+    const groupedCombinations = {};
+    
+    for (const combination of allCombinations) {
+        const groupKey = variables
+            .filter(v => !v.isRandom)
+            .map(v => combination[v.name])
+            .join('-');
+    
+        if (!groupedCombinations[groupKey]) {
+            groupedCombinations[groupKey] = [];
+        }
+        groupedCombinations[groupKey].push(combination);
+    }
+    
+    // Shuffle each group
+    for (const key in groupedCombinations) {
+        groupedCombinations[key] = shuffleArray(groupedCombinations[key]);
+    }
+    
+    // Flatten the grouped combinations into a single array
+    let finalCombinations = Object.values(groupedCombinations).flat();
+    
+    // Repeat each combination repeatPerCombination times
+    let conditions = [];
+    for (const combination of finalCombinations) {
+        for (let i = 0; i < repeatPerCombination; i++) {
+            conditions.push({ ...combination });
+        }
+    }
+    
+    // If repetition should be random, shuffle the repeated combinations
+    if (isRepetitionRandom) {
+        // Group by non-random variables first
+        const randomGroupedCombinations = {};
+        for (const combination of conditions) {
+            const groupKey = variables
+                .filter(v => !v.isRandom)
+                .map(v => combination[v.name])
+                .join('-');
+    
+            if (!randomGroupedCombinations[groupKey]) {
+                randomGroupedCombinations[groupKey] = [];
+            }
+            randomGroupedCombinations[groupKey].push(combination);
+        }
+    
+        // Shuffle within each group
+        for (const key in randomGroupedCombinations) {
+            randomGroupedCombinations[key] = shuffleArray(randomGroupedCombinations[key]);
+        }
+    
+        // Flatten the grouped combinations into a single array
+        conditions = Object.values(randomGroupedCombinations).flat();
+    }
+    
+    return conditions;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function generateCombinations(variables) {
+    const combinations = [];
+
+    function createCombination(currentCombination, currentIndex) {
+        if (currentIndex === variables.length) {
+            combinations.push({ ...currentCombination });
+            return;
+        }
+
+        const variable = variables[currentIndex];
+        for (const value of variable.values) {
+            currentCombination[variable.name] = value;
+            createCombination(currentCombination, currentIndex + 1);
+        }
+    }
+
+    createCombination({}, 0);
+
+    return combinations;
 }
