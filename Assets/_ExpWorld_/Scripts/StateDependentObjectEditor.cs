5da5d13e01a0ab56b2513d74f55a1627020ca5de
YH_hao_are_you <mlapenang1998@gmail.com>
2024-07-26 00:03:12 +0900

    add: execute editor window script only in UNITY_EDITOR mode

--- Assets/_ExpWorld_/Scripts/StateDependentObjectEditor.cs
+++ Assets/_ExpWorld_/Scripts/StateDependentObjectEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 
@@ -250,3 +251,4 @@ public class StateDependentObjectEditor : EditorWindow
         }
     }
 }
+#endif
--- Assets/_ExpWorld_/Scripts/StateListEditor.cs
+++ Assets/_ExpWorld_/Scripts/StateListEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System;
@@ -248,3 +249,4 @@ public class StateListEditor : EditorWindow
         }
     }
 }
+#endif
