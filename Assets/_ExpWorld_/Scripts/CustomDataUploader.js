701fa554cbdfd35c90207c93f67ebdaf8bd7674b
YH_hao_are_you <mlapenang1998@gmail.com>
2024-08-02 16:42:06 +0900

    feat: Custom data recorder

---
+++ Assets/_ExpWorld_/Scripts/CustomDataUploader.js
@@ -0,0 +1,36 @@
+$.onStart(() => {
+    $.state.customData = {};
+})
+
+$.onUpdate(() => {
+    if ($.getStateCompat("this", "exp_recordCustomData", "bool")) {
+        $.setStateCompat("this", "exp_recordCustomData", false);
+        recordData();
+    }
+    if ($.getStateCompat("this", "exp_uploadCustomData", "bool")) {
+        $.setStateCompat("this", "exp_uploadCustomData", false);
+        uploadData();
+    }
+})
+
+function recordData () {
+    $.state.customData = calculateData();
+}
+
+function uploadData () {
+    $.log("Send final answers: " + JSON.stringify($.state.customData));
+    let request = { type: "uploadCustomData", dataByFileName: JSON.stringify($.state.customData) };
+    $.callExternal(JSON.stringify(request), "customDataUploaded");
+}
+
+$.onExternalCallEnd((res, meta, err) =>
+{
+    if (res == null) {
+        $.log("callExternal ERROR: " + err);
+        return;
+    }
+
+    if (meta === "customDataUploaded") {
+        $.log("Custom recorded data uploaded");
+    }
+});
