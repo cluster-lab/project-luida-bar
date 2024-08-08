a0518dc1849e66776cc3ef4eb261293301329660
YH_hao_are_you <mlapenang1998@gmail.com>
2024-08-08 10:41:47 +0900

    feat: make conditions available for custom data recorder

--- Assets/_ExpWorld_/Scripts/CustomDataRecording/CustomDataCalculatorTemplate.js
+++ Assets/_ExpWorld_/Scripts/CustomDataRecording/CustomDataCalculatorTemplate.js
@@ -1,7 +1,15 @@
 function calculateData () {
     let fileName = "sampleFileName";
     // Implement your calculation for custom data to record & upload
+    // $.state.currentCondition is available if this script is combined with CustomDataUploader.js
     
     // Return the data in the following format:
-    return { ...$.state.customData, [fileName]: {} }; // $.state.customData is the custom data recorded before 
+    return { ...$.state.customData, [fileName]: {} }; // $.state.customData is the custom data recorded before
+    /*
+    e.g.
+    return { ...$.state.customData, [fileName]: {
+      ...($.state.customData[fileName] || {}),
+      labelName: calculationResult
+    } };
+    */
 }
--- Assets/_ExpWorld_/Scripts/CustomDataRecording/CustomDataUploader.js
+++ Assets/_ExpWorld_/Scripts/CustomDataRecording/CustomDataUploader.js
@@ -1,8 +1,18 @@
 $.onStart(() => {
     $.state.customData = {};
+    $.getItemsNear($.getPosition(), 0.1).forEach(item => {
+        if (item.id === "5570182165721890090") { // ConditionManager
+            item.send("exp_conditionDependentObject", true);
+        }
+    });
 })
 
 $.onUpdate(() => {
+    if ($.getStateCompat("this", "exp_conditionChanged", "boolean")) {
+        $.setStateCompat("this", "exp_conditionChanged", false);
+        $.state.currentCondition = $.state.conditions[$.getStateCompat("global", "exp_conditionID", "integer")];
+    }
+
     if ($.getStateCompat("this", "exp_recordCustomData", "boolean")) {
         $.setStateCompat("this", "exp_recordCustomData", false);
         recordData();
@@ -10,6 +20,13 @@ $.onUpdate(() => {
     if ($.getStateCompat("this", "exp_uploadCustomData", "boolean")) {
         $.setStateCompat("this", "exp_uploadCustomData", false);
         uploadData();
+    }
+})
+
+$.onReceive((messageType, arg, sender) => {
+    if (messageType === "exp_updateConditions") {
+        $.state.conditions = arg;
+        $.setStateCompat("this", "exp_conditionChanged", true);
     }
 })
 
