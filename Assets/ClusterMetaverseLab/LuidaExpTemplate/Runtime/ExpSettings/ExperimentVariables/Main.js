29d109f2f9d2c27b85329d27a7a548313d380867
YH_hao_are_you <mlapenang1998@gmail.com>
2024-10-17 17:48:29 +0900

    Add AfterTrials state to template scene

--- Assets/ClusterMetaverseLab/LuidaExpTemplate/Runtime/ExpSettings/ExperimentVariables/Main.js
+++
@@ -1,8 +0,0 @@
-const trialsCountForEachUniqueCondition = 2;
-const within_subjects_variables = [
-    { name: "color", values: ["W", "B"], isRandom: false },
-    { name: "size", values: ["10", "20", "30"], isRandom: true },
-];
-const between_subjects_variables = [
-    { name: "method", values: ["new", "old"], isRandom: false },
-];
