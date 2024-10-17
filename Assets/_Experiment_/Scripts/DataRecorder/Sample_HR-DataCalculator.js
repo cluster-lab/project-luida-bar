c80f94cb5b01b8710021747063db96090cf11026
YH_hao_are_you <mlapenang1998@gmail.com>
2024-10-17 21:17:14 +0900

    Fix data calculator template script

--- Assets/_Experiment_/Scripts/DataRecorder/Sample_HR-DataCalculator.js
+++ Assets/_Experiment_/Scripts/DataRecorder/Sample_HR-DataCalculator.js
@@ -1,13 +1,17 @@
 function calculateData () {
   let fileName = "taskAnswers";
-  return {
-      ...$.state.customData,
-      [fileName]: [
-          ...($.state.customData[fileName] || []),
-          {
-              g: $.state.currentCondition["gain"], // 現在のゲイン条件
-              a: $.getStateCompat("global", "isFaster", "boolean") ? "F" : "S" // 「速い」「遅い」のどちらを選んだか
-          }
-      ]
+  let returnData = $.state.customData;
+
+  const newRecord = {
+    g: $.state.currentCondition["gain"], // 現在のゲイン条件
+    a: $.getStateCompat("global", "isFaster", "boolean") ? "F" : "S" // 「速い」「遅い」のどちらを選んだか
   };
+
+  if (fileName in returnData && Array.isArray(returnData[fileName])) {
+    returnData[fileName].push(newRecord);
+  } else {
+    returnData[fileName] = [newRecord];
+  }
+
+  return returnData;
 }
