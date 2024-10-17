a6919f1edcc78c1faf23714972afc1cb457dfddf
YH_hao_are_you <mlapenang1998@gmail.com>
2024-10-17 21:40:14 +0900

    Remove old sample scenes and related assets

--- Assets/ClusterMetaverseLab/LuidaExpTemplate/Runtime/ExpSettings/ExperimentVariables/Sample.js
+++
@@ -1,8 +0,0 @@
-const trialsCountForEachUniqueCondition = 2;
-const within_subjects_variables = [
-    { name: "question", values: ["fontColor", "meaning"], isRandom: false },
-    { name: "wordObject", values: ["R", "G", "B"], isRandom: true },
-];
-const between_subjects_variables = [
-    { name: "lang", values: ["ja", "en"], isRandom: false },
-];
