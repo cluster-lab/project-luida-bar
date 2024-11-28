ee5ce4c3d7d846958ac91830d506b81192ef2fb3
YH_hao_are_you <mlapenang1998@gmail.com>
2024-11-28 17:15:41 +0900

    feat: use worldReferenceList to retrieve quest board

--- Assets/_Luida_/Scripts/QuestBoard.js
+++ Assets/_Luida_/Scripts/QuestBoard.js
@@ -49,9 +49,6 @@ $.onReceive((messageType, arg, sender) => {
         case "quest_board_to_prev":
             toPrev();
             break;
-        case "get_quest_board":
-            sender.send("return_quest_board", true);
-            break;
         case "quest_board_get_current_page":
             sender.send("quest_board_current_page", $.state.currentPage);
             break;
