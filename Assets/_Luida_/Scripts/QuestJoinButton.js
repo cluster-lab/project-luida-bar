905f85466ad7c2426093017715c448d79b02df58
YH_hao_are_you <mlapenang1998@gmail.com>
2024-11-28 15:07:17 +0900

    fix: change routing keys for world gates

--- Assets/_Luida_/Scripts/QuestJoinButton.js
+++ Assets/_Luida_/Scripts/QuestJoinButton.js
@@ -1,6 +1,6 @@
 $.onInteract(() => {
     const currentQuestID = $.getStateCompat("owner", "currentQuestID", "integer") + 1;
     if (currentQuestID && currentQuestID > 0) {
-        $.setStateCompat("owner", "DropWorldGate" + ("00" + currentQuestID).slice(-2), true);
+        $.setStateCompat("owner", "DropWorldGate" + currentQuestID, true);
     }
 })
