b9ae3daf7e53b198336ab1b47cc258e782a7ef4d
YH_hao_are_you <mlapenang1998@gmail.com>
2025-01-26 02:16:45 +0900

    Define recording custom data

--- Assets/_Experiment_/Scripts/DataRecorder/HandRedirection.js
+++ Assets/_Experiment_/Scripts/DataRecorder/HandRedirection.js
@@ -1,5 +1,20 @@
 function calculateData () {
-    let returnData = $.state.customData;
-    const CONDITION = $.groupState.currentCondition;
-    return returnData;
+  let returnData = $.state.customData;
+  const CONDITION = $.groupState.currentCondition;
+
+  function saveData_taskAnswer() {
+return {
+  gain: CONDITION["gain"],
+  isFaster: $.getStateCompat("global", "isFaster", "boolean");
+};
+    return {};
+  }
+  const newRecord_taskAnswer = saveData_taskAnswer();
+  if ("taskAnswer" in returnData && Array.isArray(returnData["taskAnswer"])) {
+    returnData["taskAnswer"].push(newRecord_taskAnswer);
+  } else {
+    returnData["taskAnswer"] = [newRecord_taskAnswer];
+  }
+
+  return returnData;
 }
