770ebd694a28216d1be7b579401f676f5face821
YH_hao_are_you <mlapenang1998@gmail.com>
2025-01-27 23:17:44 +0900

    Batch upload custom data

--- Assets/ClusterMetaverseLab/LuidaExpTemplate/Runtime/Scripts/CustomDataRecording/CustomDataUploader.js
+++ Assets/ClusterMetaverseLab/LuidaExpTemplate/Runtime/Scripts/CustomDataRecording/CustomDataUploader.js
@@ -1,15 +1,31 @@
+const uploadInterval = 1;
+const dataLengthPerUpload = 15;
+
 $.onStart(() => {
     $.state.customData = {};
+    $.state.uploadIndex = 0;
+    $.state.elapsedTime = 0;
+    $.state.dataLength = 0;
+    $.state.isUploading = false;
 })
 
-$.onUpdate(() => {
+$.onUpdate((deltaTime) => {
     if ($.getStateCompat("this", "exp_recordCustomData", "boolean")) {
         $.setStateCompat("this", "exp_recordCustomData", false);
         recordData();
     }
     if ($.getStateCompat("this", "exp_uploadCustomData", "boolean")) {
-        $.setStateCompat("this", "exp_uploadCustomData", false);
-        uploadData();
+        if ($.state.uploadIndex === 0) {
+            uploadDataInit();
+            $.setStateCompat("this", "exp_uploadCustomData", false);
+        }
+    }
+    if ($.state.isUploading) {
+        $.state.elapsedTime = $.state.elapsedTime + deltaTime;
+        if ($.state.elapsedTime >= uploadInterval && $.state.uploadIndex < Math.ceil($.state.dataLength / dataLengthPerUpload)) {
+            $.state.elapsedTime = 0;
+            uploadDataStep();
+        }
     }
 })
 
@@ -17,16 +33,41 @@ function recordData () {
     $.state.customData = calculateData();
 }
 
-function uploadData () {
-    $.log("Upload custom data: " + JSON.stringify($.state.customData));
-    let request = {
-        type: "uploadCustomData",
-        token: token || "",
-        data: $.state.customData,
-        eID: expID || "",
-        pID: $.getPlayersNear($.getPosition().clone(), Infinity)[0].idfc || "" // TODO: retrieve idfc through cluster Player Script
-    };
-    $.callExternal(JSON.stringify(request), "customDataUploaded");
+function uploadDataInit() {
+    $.state.uploadIndex = 0;
+    $.state.elapsedTime = 0;
+    $.state.isUploading = true;
+    let firstFileName = Object.keys($.state.customData)[0];
+    if (firstFileName) {
+        $.state.dataLength = $.state.customData[firstFileName].length;
+    }
+}
+
+function uploadDataStep() {
+    $.log($.state.uploadIndex);
+    if ($.state.uploadIndex < Math.ceil($.state.dataLength / dataLengthPerUpload)) {
+        const slicedData = Object.fromEntries(
+            Object.entries($.state.customData).map(([key, value]) => [
+                key,
+                value.slice($.state.uploadIndex * dataLengthPerUpload, ($.state.uploadIndex + 1) * dataLengthPerUpload)
+            ])
+        );
+        let request = {
+            type: "uploadCustomData",
+            token: token || "",
+            data: slicedData,
+            eID: expID || "",
+            pID: $.getPlayersNear($.getPosition().clone(), Infinity)[0].idfc || "" // TODO: retrieve idfc through cluster Player Script
+        };
+        $.log(request);
+        $.callExternal(JSON.stringify(request), "customDataUploaded");
+        $.state.uploadIndex = $.state.uploadIndex + 1;
+
+        if ($.state.uploadIndex >= Math.ceil($.state.dataLength / dataLengthPerUpload)) {
+            $.state.uploadIndex = 0;
+            $.state.isUploading = false;
+        }
+    }
 }
 
 $.onExternalCallEnd((res, meta, err) =>
