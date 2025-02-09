382a1d7608fb2663136c9a4ae45c2c5985fabd7a
YH_hao_are_you <mlapenang1998@gmail.com>
2025-02-09 15:52:44 +0900

    Add default answer options for linear scale if answer options are empty

--- Assets/ClusterMetaverseLab/LuidaExpTemplate/Runtime/Scripts/Form/FormController.js
+++ Assets/ClusterMetaverseLab/LuidaExpTemplate/Runtime/Scripts/Form/FormController.js
@@ -67,6 +67,9 @@ function initQuestion() {
     $.state.answerOptions = Array.isArray(q.a)
         ? q.a
         : (typeof q.a === "string" ? q.a.split(",") : []) ;
+    if (q.i === 1 && $.state.answerOptions.length === 0) {
+        $.state.answerOptions = [1,2,3,4,5,6,7];
+    }
     if ($.state.answerOptions.length === 0) $.state.answerOptions = [""];
     spawnAnswerOptionUI();
 }
