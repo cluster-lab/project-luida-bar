ff1bae5f263f1dd18ec85c8652fde3a7e63a0e43
YH_hao_are_you <mlapenang1998@gmail.com>
2025-10-05 16:20:23 +0900

    Minimal fix for hand redirection sample experiment

--- Assets/_Experiment_/Settings/ExperimentVariables/HandRedirection.js
+++ Assets/_Experiment_/Settings/ExperimentVariables/HandRedirection.js
@@ -1,6 +1,6 @@
 const trialsCountForEachUniqueCondition = 2;
 const within_subjects_variables = [
-    { name: "gain", values: ["0.75", "0.8", "0.85", "0.9", "0.95", "1", "1.05", "1.1", "1.15", "1.2", "1.25"], isRandom: true },
+    { name: "gain", values: ["0.7", "0.8", "0.9", "1", "1.1", "1.2", "1.3"], isRandom: true },
 ];
 const between_subjects_variables = [
 ];
