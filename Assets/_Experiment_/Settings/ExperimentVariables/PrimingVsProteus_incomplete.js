0492858f7d9c29d232adb656f4c508931f82ea0c
YH_hao_are_you <mlapenang1998@gmail.com>
2026-05-21 22:05:48 +0900

    Remove unused sample scenes

--- Assets/_Experiment_/Settings/ExperimentVariables/PrimingVsProteus_incomplete.js
+++
@@ -1,8 +0,0 @@
-const trialsCountForEachUniqueCondition = 1;
-const within_subjects_variables = [
-];
-const between_subjects_variables = [
-    { name: "avatar", values: ["young", "elder"], isRandom: true, debugValue: null },
-    { name: "video", values: ["young", "elder"], isRandom: true, debugValue: null },
-];
-const state_names = ["Start", "Avatar", "Video", "Trial - Start", "Trial - Rest", "Outro", "End"];
